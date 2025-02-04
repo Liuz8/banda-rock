aaa7b2ad2effd69e544f19cb1b270bbe11d09906
Luis <luismarfran08@gmail.com>
2025-02-04 01:43:54 -0600

    termina la funcion de mostrar y desaparecer de la informacion de cada foto

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-document.getElementById("buton").addEventListener("click", function() {
+document.getElementById("buton-luis").addEventListener("click", function() {
     let div = document.querySelector(".inf_luis");
+    div.classList.toggle("mostrar");
+});
+
+document.getElementById("buton-oscar").addEventListener("click", function() {
+    let div = document.querySelector(".inf_oscar");
+    div.classList.toggle("mostrar");
+});
+
+document.getElementById("buton-erick").addEventListener("click", function() {
+    let div = document.querySelector(".inf_erick");
+    div.classList.toggle("mostrar");
+});
+
+document.getElementById("buton-daniel").addEventListener("click", function() {
+    let div = document.querySelector(".inf_daniel");
     div.classList.toggle("mostrar");
 });
